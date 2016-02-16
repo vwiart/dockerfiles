136e7e5477a6b9d44750fde6f7b47abed72728ee
Vincent Wiart <vincent.wiart@gmail.com>
2016-02-16 10:32:39 +0100

    Removing useless images

--- nginx-proxy/start.sh
+++ nginx-proxy/start.sh
@@ -6,8 +6,6 @@ sudo docker run -d \
 	-v $(pwd)/conf.d:/etc/nginx/conf.d:ro \
 	-v $(pwd)/static:/var/www/html/static \
 	--link wordpress-blue \
-	--link synapps-io \
-	--link synapps-ambari \
 	--link hadoop \
 	--name $DOCKER_TAG \
 	-t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
