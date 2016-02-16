9033e1debbdcf5d29381f679a7e121c1c1aef766
Vincent Wiart <vincent.wiart@gmail.com>
2016-02-16 10:30:12 +0100

    Update on base-debian : adding vim deps

--- base-debian/build.sh
+++ base-debian/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 . ./env.sh
 
-if [[ $1 == "--no-cache" ]]; then
-	sudo docker build --no-cache -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION .
-else
-	sudo docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION .
-fi
+sudo docker build --no-cache -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION .
