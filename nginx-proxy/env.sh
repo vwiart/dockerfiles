0f8617be6316cff9e7e1647f8830ede13f0bb67b
Vincent Wiart <vincent.wiart@gmail.com>
2016-02-18 09:33:27 +0100

    Removing link & using networks

--- nginx-proxy/env.sh
+++ nginx-proxy/env.sh
@@ -2,3 +2,4 @@
 DOCKER_IMAGE_NAME=vwiart/nginx-proxy
 DOCKER_IMAGE_VERSION=1.0.1
 DOCKER_TAG=overlord
+NETWORK_NAME=synapps
--- nginx-proxy/start.sh
+++ nginx-proxy/start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 . ./env.sh
 
+function chkNetwork() {
+        sudo docker network inspect $NETWORK_NAME &> /dev/null
+        if [ $? == 1 ]; then
+		echo "creating network $NETWORK_NAME"
+                sudo docker network create --driver bridge $NETWORK_NAME
+        fi
+}
+chkNetwork
+
 sudo docker run -d \
 	-p 80:80 \
 	-v $(pwd)/conf.d:/etc/nginx/conf.d:ro \
-	-v $(pwd)/static:/var/www/html/static \
-	--link wordpress-blue \
-	--link hadoop \
+	-v $(pwd)/static:/var/www/html/static:ro \
+	--net=synapps \
 	--name $DOCKER_TAG \
 	-t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
